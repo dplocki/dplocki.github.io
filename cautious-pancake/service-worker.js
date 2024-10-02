12103cfc613c0d1553b1be3206ffd110b8809082
Dawid Plocki <dawid.plocki@gmail.com>
2024-10-02 17:07:20 +0200

    Upload Cautious Pancake v.1.0

---
+++ cautious-pancake/service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'cautious-pancake-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/main.js',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  '/apple-touch-icon.png',
+  '/favicon-16x16.png',
+  '/favicon-32x32.png',
+  '/favicon.ico',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
